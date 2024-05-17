b5347d4b9752975e0859e18f1a12bad65c7d2482
Marko <marko.mittal@gmail.com>
2024-05-17 14:26:15 +0300

    Add logo to login and register page

--- login.php
+++ login.php
@@ -1,5 +1,12 @@
 <?php include 'elements/head.php'; ?>
     <div class="row">
+        <div class="col-12">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="static/img/logo.png" alt="logo" class="img-fluid w-50">
+            </div>
+        </div>
+    </div>
+    <div class="row">
     <form action="db/login.php" method="post">
         <div class="col-12">
             <div class="w-100 d-flex align-items-center justify-content-center">
